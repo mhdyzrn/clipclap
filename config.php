75de43c4ef092dc7a66d500ec88a41cf891e972a
mhdyzrn <51892371+mhdyzrn@users.noreply.github.com>
2020-07-21 21:15:45 +0700

    Add files via upload

---
+++ config.php
@@ -0,0 +1,7 @@
+<?php
+
+$eksternal_version = "1.9.6.1";
+$version = "41";
+$token = "YWJiMGRhYjQtODllYy00NDZiLWE5YWEtZjMyMDViNjYyNjFh";
+$uuid = "71847c1b-ac03-4f03-8d35-93e8d86622bd";
+$userid = "17052640";
