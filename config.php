4581f67f857e5b1ccc87e5de862db87e7dd2b894
mhdyzrn <51892371+mhdyzrn@users.noreply.github.com>
2020-08-15 19:47:44 +0700

    Update config.php

--- config.php
+++ config.php
@@ -2,6 +2,6 @@
 
 $eksternal_version = "1.9.8";
 $version = "42";
-$token = "NTU5YjNmOTItMjI2MC00Mzc5LTg1NDMtOGRhOTczNGNkYjg2";
+$token = "ZTg2ZTVmNDUtZmEyNC00ZmQ0LWJhNzktZTEzMGY4MTlmMWYz";
 $uuid = "fc0d3371-3bb5-4765-b239-e8bbe67e153b";
 $userid = "17052640";
